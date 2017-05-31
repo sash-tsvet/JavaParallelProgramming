c997ee22bdb00f3523c93e3ad3dbfcb3fd6ef8df
sash-tsvet <sash.tsvet@gmail.com>
2017-05-31 04:45:40 +0300

    Task 8 added

--- Ratvin8/src/Solution.java
+++ Ratvin8/src/Solution.java
@@ -1,24 +1,57 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 /**
  * Created by kitty on 31.05.2017.
  */
 public class Solution {
-//    List<File> files = new ArrayList<>();
-//    void print(File file, int depth) {
-//        System.out.println(file.getName()+ depth);
-//        depth++;
-//        if (file.isDirectory()) {
-//            for (File nested : file:list()){
-//                files.add(nested);
-//                print(nested, depth);
-//            }
-//        }
-//    }
-
     public static void main(String[] args) {
+        File rootDir = new File("C:\\Users\\kitty\\IdeaProjects");
+        List<String> result = new ArrayList<>();
+        Queue<File> fileTree = new PriorityQueue<>();
 
+        Collections.addAll(fileTree, rootDir.listFiles());
+
+        while (!fileTree.isEmpty())
+        {
+            File currentFile = fileTree.remove();
+            if(currentFile.isDirectory()){
+                Collections.addAll(fileTree, currentFile.listFiles());
+            } else {
+                result.add(currentFile.getAbsolutePath());
+            }
+        }
+        for (String element : result) {
+//            System.out.println(element);
+            new Thread(new Worker(element)).start();
+        }
+    }
+
+    public static class Worker implements Runnable {
+        String path;
+        Worker(String path){
+            this.path = path;
+        }
+
+        @Override
+        public void run() {
+            Charset charset = StandardCharsets.UTF_8;
+            String search = "implements";
+            String replace = "**********";
+            try {
+//                System.out.println(new String(Files.readAllBytes(Paths.get(path))).contains(search.toLowerCase()));
+                if (new String(Files.readAllBytes(Paths.get(path))).contains(search.toLowerCase())) {
+//                    System.out.println(new String(Files.readAllBytes(Paths.get(path))).replace(search, replace));
+                    System.out.println(path);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
